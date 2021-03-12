9d4033d5cd387b8e19189c11b9438d3f2c520566
Przemczak <przemyslaw.wojtczak@outlook.com>
2021-03-12 15:40:17 +0100

    Add project files.

---
+++ EncryptionApp/EncryptClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EncryptionApp
+{
+    public class EncryptClass
+    {
+        public void WordsEncrypt(List<string>WordsList, string OutputWordsFilePath, string OutputFolder)
+        {
+            try
+            {
+                List<string> EncryptedWords = new List<string>();
+
+                for (int i = 0; i < WordsList.Count; i++)
+                {
+                    char[] array = WordsList[i].ToArray();
+                    for (int ii = 0; ii < array.Length; ii++)
+                    {
+                        if (array[ii] != 'z')
+                        {
+                            array[ii] = (char)(array[ii] + 1);
+                        }
+                    }
+                    string OutputWord = new string(array);
+                    EncryptedWords.Add(OutputWord);
+                }
+
+                Console.WriteLine("Przekonwertowano slow: " + EncryptedWords.Count);
+                SaveOutputFile(OutputWordsFilePath, EncryptedWords, OutputFolder);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey();
+                return;
+            }
+        }
+
+
+        private void SaveOutputFile(string OutputWordsFilePath, List<string> EncryptedWords, string OutputFolder)
+        {
+            try
+            {
+                Directory.CreateDirectory(OutputFolder);
+
+                using (StreamWriter streamWriter = new StreamWriter(OutputWordsFilePath))
+                {
+                    foreach (string word in EncryptedWords)
+                        streamWriter.WriteLine(word);
+                };
+                Console.WriteLine("\nSukces! Plik zapisano w: " + OutputWordsFilePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
---
+++ EncryptionApp/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!File.Exists(Assembly.GetEntryAssembly().Location + ".config"))
+            {
+                Console.WriteLine("Brak pliku konfiguracyjnego.");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                //Get settings from config file
+                string InputFolder = ConfigurationManager.AppSettings.Get("InputFolder");
+                string OutputFolder = ConfigurationManager.AppSettings.Get("OutputFolder");
+                string InputWordsFile = ConfigurationManager.AppSettings.Get("WordsFile");
+                string InputWordsExceptionFile = ConfigurationManager.AppSettings.Get("WordsExceptionFile");
+
+                //Create variables
+                string WordsFile = Path.Combine(InputFolder, InputWordsFile);
+                string WordsExceptionFile = Path.Combine(InputFolder, InputWordsExceptionFile);
+                string OutputWordsFileName = "Wynik_" + DateTime.Now.ToString("dd.MM.yyyy_HHmmss") + ".txt";
+                string OutputWordsFilePath = Path.Combine(OutputFolder, OutputWordsFileName);
+
+                if(!File.Exists(WordsFile))
+                {
+                    Console.WriteLine("Brak pliku ze slowami lub bledny format.");
+                    Console.ReadKey();
+                    return;
+                }
+                else if(!File.Exists(WordsExceptionFile))
+                {
+                    Console.WriteLine("Brak pliku z wyjatkami lub bledny format.");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    LoadFiles(WordsFile, WordsExceptionFile, OutputWordsFilePath, OutputFolder);
+                }
+            }
+
+            void LoadFiles(string WordsFile, string WordsExceptionFile, string OutputWordsFilePath, string OutputFolder)
+            {
+                try
+                {
+                    Regex regexDelim = new Regex(@"[:\/\s\""\,\.\-\;\:]+");
+                    List<string> WordsExceptionList = new List<string>();
+                    List<string> WordsList = new List<string>();
+
+
+                    //Read exception words from file
+                    using ( StreamReader streamReader = new StreamReader(WordsExceptionFile))
+                    {
+                        string WordsTemp = streamReader.ReadToEnd();
+                        WordsExceptionList = regexDelim.Split(WordsTemp).ToList();
+                    };
+                    Console.WriteLine("Wczytano slow: " + WordsExceptionList.Count);
+
+                    //Read exception words from file
+                    using (StreamReader streamReader = new StreamReader(WordsFile))
+                    {
+                        string WordsTemp = streamReader.ReadToEnd();
+                        WordsList = regexDelim.Split(WordsTemp).ToList();
+                    };
+                    Console.WriteLine("Wczytano wyjatkow: " + WordsList.Count);
+
+                    WordsList = WordsList.Except(WordsExceptionList).ToList();
+                    Console.WriteLine("\nWyrazy z wyjatkami:  " + WordsList.Count);
+
+                    if (WordsList.Count == 0)
+                    {
+                        Console.WriteLine("Brak slow do zaszyfrowania.");
+                        Console.ReadKey();
+                        return;
+                    }
+                    else
+                    {
+                        EncryptClass encryptClass = new EncryptClass();
+                        encryptClass.WordsEncrypt(WordsList, OutputWordsFilePath, OutputFolder);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
